ea4455002652ed353bb4b516bb9863f0d9e406b1
Andrew McGuffin <andrew.m.mcguffin-1@ou.edu>
2019-09-30 12:28:34 -0700

    Exam 1 Questions

---
+++ ExamQs/src/ExamQuestions.java
@@ -0,0 +1,8 @@
+
+public class ExamQuestions {
+	1. what is the main purpose of the "finally" command?
+			A. to clean up resources and close things
+	2. show how to create a new object of type "Object"
+	3. is this valid code?
+			A. Yes or No
+}
