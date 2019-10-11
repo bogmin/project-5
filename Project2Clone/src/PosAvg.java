0d36d48681156d55d193957a7765974e12994d5a
Andrew McGuffin <andrew.m.mcguffin-1@ou.edu>
2019-10-10 17:45:08 -0700

    posavg constructor

--- Project2Clone/src/PosAvg.java
+++ Project2Clone/src/PosAvg.java
@@ -1,4 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class PosAvg {
+	int numStations = 0;
+	String stID;
+	private ArrayList<String> stations;
+	
+	public PosAvg(String stID) throws IOException {
+		this.stID = stID; 
+		this.stations = new ArrayList<String>();
+		addStationNames();
+	}
 
 }
