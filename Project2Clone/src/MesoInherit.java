79734a15a7561fb694070d1c06bdddf6288e1b0e
Andrew McGuffin <andrew.m.mcguffin-1@ou.edu>
2019-10-10 17:18:55 -0700

    MesoInherit constructor n stuff

--- Project2Clone/src/MesoInherit.java
+++ Project2Clone/src/MesoInherit.java
@@ -1,4 +1,9 @@
 
-public class MesoInherit {
-
+public class MesoInherit extends MesoAbstract {
+	MesoStation station;
+	private int[] proper = new int[3];
+	
+	public MesoInherit(MesoStation mesoStation) {
+		this.station = mesoStation;
+	}
 }
