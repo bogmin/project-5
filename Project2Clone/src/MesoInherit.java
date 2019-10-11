9e562f5f98d0ca1916f2210dda9c3948915682f8
Andrew McGuffin <andrew.m.mcguffin-1@ou.edu>
2019-10-10 17:33:32 -0700

    calAverage filled out

--- Project2Clone/src/MesoInherit.java
+++ Project2Clone/src/MesoInherit.java
@@ -9,7 +9,22 @@ public class MesoInherit extends MesoAbstract {
 	
 	@Override
 	public int[] calAverage() {
+		int[] values = new int[station.getStID().length()];
+		double avg = 0;
 		
+		for(int i = 0; i < station.getStID().length(); ++i) {
+			char letter = station.getStID().charAt(i);
+			int ascii = (int) letter;
+			values[i] = ascii;
+		}
+		for(int i = 0; i < values.length; ++i) {
+			avg = avg + values[i];
+		}
+		avg = avg / values.length;
+		proper[0] = (int) Math.ceil(avg);
+		proper[1] = (int) Math.floor(avg);
+		proper[2] = (int) Math.round(avg);
+		return proper;
 	}
 	@Override
 	public char letterAverage() {
