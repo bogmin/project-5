3e53eb49d79210410ff8da116fc5bb1afec40f65
Andrew McGuffin <andrew.m.mcguffin-1@ou.edu>
2019-10-10 17:15:13 -0700

    abstracts made

--- Project2Clone/src/MesoAbstract.java
+++ Project2Clone/src/MesoAbstract.java
@@ -1,4 +1,14 @@
+/**
+ * @author Mohammad Mukhtaruzzaman
+ * @version 2019-09-18
+*/
+public abstract class MesoAbstract 
+{
+	//Abstract method calAverage to return integer array.
+	abstract int[] calAverage();
+	
+	abstract char letterAverage();
 
-public class MesoAbstract {
-
+	//TODO: Create an abstract method letterAverage with return type char
+	
 }
